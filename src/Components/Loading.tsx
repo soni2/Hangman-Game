524e2dea4441ecfd82c62449ce0df7a2f23de02f
reyescode <soni2gf@gmail.com>
2025-02-03 13:32:51 -0400

    fixing loading

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,6 +1,22 @@
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 export default function Loading() {
+  const [dots, setDots] = useState(0);
+
+  const dotsArray = [".", "..", "..."];
+
+  useEffect(() => {
+    const dotsInterval = setInterval(() => {
+      setDots(dots + 1);
+      if (dots === 3) {
+        setDots(0);
+      }
+    }, 1000);
+
+    return () => clearInterval(dotsInterval);
+  }, [dots]);
+
   return (
     <motion.div
       initial={{ opacity: 0, y: 10 }}
@@ -9,7 +25,9 @@ export default function Loading() {
       className="flex items-center justify-center flex-col"
     >
       <div className="grid grid-flow-col gap-8 pb-4">
-        <h1 className="text-red-600">Cargando...</h1>
+        <h1 className="text-dark-blue dark:text-white text-4xl">
+          Cargando{dotsArray[dots]}
+        </h1>
       </div>
     </motion.div>
   );
