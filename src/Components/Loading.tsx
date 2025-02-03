fa3f47708cd33e1c3bc653156e1983154ae9442e
reyescode <soni2gf@gmail.com>
2025-02-03 13:19:36 -0400

    fixing loading

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,9 +1,16 @@
+import { motion } from "framer-motion";
+
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center flex-col">
+    <motion.div
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="flex items-center justify-center flex-col"
+    >
       <div className="grid grid-flow-col gap-8 pb-4">
         <h1 className="text-red-600">Cargando...</h1>
       </div>
-    </div>
+    </motion.div>
   );
 }
